b59cca8897cf95310a6ed41d89a3f9301a2c6449
Marcelo Mira <mmira@machinalis.com>
2015-07-18 15:35:23 -0300

    bootswatch y abm

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('dish', function() {
+  this.resource('dish', function() {
     this.route('edit');
     this.route('new');
   });
